bc36f2b0c8998296be46e776c0883dd4ca322425
mrhanihere <143721493+mrhanihere@users.noreply.github.com>
2023-08-31 18:34:54 +0500

    Add files via upload

---
+++ Random Password Generator in Python.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+length = int(input("Enter the password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for i in range(length))
+print("Random Password:", password)
